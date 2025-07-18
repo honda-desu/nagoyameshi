c78ebbf80473cf31e848fd87f863421fe81967f6
honda-desu <h9.k.honda@gmail.com>
2025-07-18 10:53:21 +0900

    add feature favorite

--- src/main/java/com/example/nagoyameshi/controller/RestaurantController.java
+++ src/main/java/com/example/nagoyameshi/controller/RestaurantController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,101 +17,139 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.nagoyameshi.entity.Category;
+import com.example.nagoyameshi.entity.Favorite;
 import com.example.nagoyameshi.entity.Restaurant;
+import com.example.nagoyameshi.entity.User;
+import com.example.nagoyameshi.security.UserDetailsImpl;
 import com.example.nagoyameshi.service.CategoryService;
+import com.example.nagoyameshi.service.FavoriteService;
 import com.example.nagoyameshi.service.RestaurantService;
 
 @Controller
 @RequestMapping("/restaurants")
 public class RestaurantController {
-   private final RestaurantService restaurantService;
-   private final CategoryService categoryService;
+	private final RestaurantService restaurantService;
+	private final CategoryService categoryService;
+	private final FavoriteService favoriteService;
 
-   public RestaurantController(RestaurantService restaurantService, CategoryService categoryService) {
-       this.restaurantService = restaurantService;
-       this.categoryService = categoryService;
-   }
+	public RestaurantController(RestaurantService restaurantService, CategoryService categoryService,
+			FavoriteService favoriteService) {
+		this.restaurantService = restaurantService;
+		this.categoryService = categoryService;
+		this.favoriteService = favoriteService;
 
-   @GetMapping
-   public String index(@RequestParam(name = "keyword", required = false) String keyword,
-                       @RequestParam(name = "categoryId", required = false) Integer categoryId,
-                       @RequestParam(name = "price", required = false) Integer price,
-                       @RequestParam(name = "order", required = false) String order,
-                       @PageableDefault(page = 0, size = 15, sort = "id", direction = Direction.ASC) Pageable pageable,
-                       Model model)
-   {
-       Page<Restaurant> restaurantPage;
+	}
 
-       if (keyword != null && !keyword.isEmpty()) {
-           if (order != null && order.equals("lowestPriceAsc")) {
-               restaurantPage = restaurantService.findRestaurantsByNameLikeOrAddressLikeOrCategoryNameLikeOrderByLowestPriceAsc(keyword, keyword, keyword, pageable);
-           }  else if (order != null && order.equals("ratingDesc")) {
-               restaurantPage = restaurantService.findRestaurantsByNameLikeOrAddressLikeOrCategoryNameLikeOrderByAverageScoreDesc(keyword, keyword, keyword, pageable);
-           } else if (order != null && order.equals("popularDesc")) {
-               restaurantPage = restaurantService.findRestaurantsByNameLikeOrAddressLikeOrCategoryNameLikeOrderByReservationCountDesc(keyword, keyword, keyword, pageable);                
-           }else {
-               restaurantPage = restaurantService.findRestaurantsByNameLikeOrAddressLikeOrCategoryNameLikeOrderByCreatedAtDesc(keyword, keyword, keyword, pageable);
-           }
-       } else if (categoryId != null) {
-           if (order != null && order.equals("lowestPriceAsc")) {
-               restaurantPage = restaurantService.findRestaurantsByCategoryIdOrderByLowestPriceAsc(categoryId, pageable);
-           } else if (order != null && order.equals("ratingDesc")) {
-               restaurantPage = restaurantService.findRestaurantsByCategoryIdOrderByAverageScoreDesc(categoryId, pageable);
-           } else if (order != null && order.equals("popularDesc")) {
-               restaurantPage = restaurantService.findRestaurantsByCategoryIdOrderByReservationCountDesc(categoryId, pageable);                
-           } else {
-               restaurantPage = restaurantService.findRestaurantsByCategoryIdOrderByCreatedAtDesc(categoryId, pageable);
-           }
-       } else if (price != null) {
-           if (order != null && order.equals("lowestPriceAsc")) {
-               restaurantPage = restaurantService.findRestaurantsByLowestPriceLessThanEqualOrderByLowestPriceAsc(price, pageable);
-           } else if (order != null && order.equals("ratingDesc")) {
-               restaurantPage = restaurantService.findRestaurantsByLowestPriceLessThanEqualOrderByAverageScoreDesc(price, pageable); 
-           } else if (order != null && order.equals("popularDesc")) {
-               restaurantPage = restaurantService.findRestaurantsByLowestPriceLessThanEqualOrderByReservationCountDesc(price, pageable);                
-           } else {
-               restaurantPage = restaurantService.findRestaurantsByLowestPriceLessThanEqualOrderByCreatedAtDesc(price, pageable);
-           }
-       } else {
-           if (order != null && order.equals("lowestPriceAsc")) {
-               restaurantPage = restaurantService.findAllRestaurantsByOrderByLowestPriceAsc(pageable);
-           } else if (order != null && order.equals("ratingDesc")) {
-               restaurantPage = restaurantService.findAllRestaurantsByOrderByAverageScoreDesc(pageable);
-           } else if (order != null && order.equals("popularDesc")) {
-               restaurantPage = restaurantService.findAllRestaurantsByOrderByReservationCountDesc(pageable);                
-           } else {
-               restaurantPage = restaurantService.findAllRestaurantsByOrderByCreatedAtDesc(pageable);
-           }
-       }
+	@GetMapping
+	public String index(@RequestParam(name = "keyword", required = false) String keyword,
+			@RequestParam(name = "categoryId", required = false) Integer categoryId,
+			@RequestParam(name = "price", required = false) Integer price,
+			@RequestParam(name = "order", required = false) String order,
+			@PageableDefault(page = 0, size = 15, sort = "id", direction = Direction.ASC) Pageable pageable,
+			Model model) {
+		Page<Restaurant> restaurantPage;
 
-       List<Category> categories = categoryService.findAllCategories();
-       model.addAttribute("restaurantPage", restaurantPage);
-       model.addAttribute("categories", categories);
-       model.addAttribute("keyword", keyword);
-       model.addAttribute("categoryId", categoryId);
-       model.addAttribute("price", price);
-       model.addAttribute("order", order);
+		if (keyword != null && !keyword.isEmpty()) {
+			if (order != null && order.equals("lowestPriceAsc")) {
+				restaurantPage = restaurantService
+						.findRestaurantsByNameLikeOrAddressLikeOrCategoryNameLikeOrderByLowestPriceAsc(keyword, keyword,
+								keyword, pageable);
+			} else if (order != null && order.equals("ratingDesc")) {
+				restaurantPage = restaurantService
+						.findRestaurantsByNameLikeOrAddressLikeOrCategoryNameLikeOrderByAverageScoreDesc(keyword,
+								keyword, keyword, pageable);
+			} else if (order != null && order.equals("popularDesc")) {
+				restaurantPage = restaurantService
+						.findRestaurantsByNameLikeOrAddressLikeOrCategoryNameLikeOrderByReservationCountDesc(keyword,
+								keyword, keyword, pageable);
+			} else {
+				restaurantPage = restaurantService
+						.findRestaurantsByNameLikeOrAddressLikeOrCategoryNameLikeOrderByCreatedAtDesc(keyword, keyword,
+								keyword, pageable);
+			}
+		} else if (categoryId != null) {
+			if (order != null && order.equals("lowestPriceAsc")) {
+				restaurantPage = restaurantService.findRestaurantsByCategoryIdOrderByLowestPriceAsc(categoryId,
+						pageable);
+			} else if (order != null && order.equals("ratingDesc")) {
+				restaurantPage = restaurantService.findRestaurantsByCategoryIdOrderByAverageScoreDesc(categoryId,
+						pageable);
+			} else if (order != null && order.equals("popularDesc")) {
+				restaurantPage = restaurantService.findRestaurantsByCategoryIdOrderByReservationCountDesc(categoryId,
+						pageable);
+			} else {
+				restaurantPage = restaurantService.findRestaurantsByCategoryIdOrderByCreatedAtDesc(categoryId,
+						pageable);
+			}
+		} else if (price != null) {
+			if (order != null && order.equals("lowestPriceAsc")) {
+				restaurantPage = restaurantService.findRestaurantsByLowestPriceLessThanEqualOrderByLowestPriceAsc(price,
+						pageable);
+			} else if (order != null && order.equals("ratingDesc")) {
+				restaurantPage = restaurantService
+						.findRestaurantsByLowestPriceLessThanEqualOrderByAverageScoreDesc(price, pageable);
+			} else if (order != null && order.equals("popularDesc")) {
+				restaurantPage = restaurantService
+						.findRestaurantsByLowestPriceLessThanEqualOrderByReservationCountDesc(price, pageable);
+			} else {
+				restaurantPage = restaurantService.findRestaurantsByLowestPriceLessThanEqualOrderByCreatedAtDesc(price,
+						pageable);
+			}
+		} else {
+			if (order != null && order.equals("lowestPriceAsc")) {
+				restaurantPage = restaurantService.findAllRestaurantsByOrderByLowestPriceAsc(pageable);
+			} else if (order != null && order.equals("ratingDesc")) {
+				restaurantPage = restaurantService.findAllRestaurantsByOrderByAverageScoreDesc(pageable);
+			} else if (order != null && order.equals("popularDesc")) {
+				restaurantPage = restaurantService.findAllRestaurantsByOrderByReservationCountDesc(pageable);
+			} else {
+				restaurantPage = restaurantService.findAllRestaurantsByOrderByCreatedAtDesc(pageable);
+			}
+		}
 
-       return "restaurants/index";
-   }
-   
-   @GetMapping("/{id}")
-   public String show(@PathVariable(name = "id") Integer id,
-                      RedirectAttributes redirectAttributes,
-                      Model model)
-   {
-       Optional<Restaurant> optionalRestaurant  = restaurantService.findRestaurantById(id);
+		List<Category> categories = categoryService.findAllCategories();
+		model.addAttribute("restaurantPage", restaurantPage);
+		model.addAttribute("categories", categories);
+		model.addAttribute("keyword", keyword);
+		model.addAttribute("categoryId", categoryId);
+		model.addAttribute("price", price);
+		model.addAttribute("order", order);
 
-       if (optionalRestaurant.isEmpty()) {
-           redirectAttributes.addFlashAttribute("errorMessage", "店舗が存在しません。");
+		return "restaurants/index";
+	}
 
-           return "redirect:/restaurants";
-       }
+	@GetMapping("/{id}")
+	public String show(@PathVariable(name = "id") Integer id,
+			@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
 
-       Restaurant restaurant = optionalRestaurant.get();
-       model.addAttribute("restaurant", restaurant);
+			RedirectAttributes redirectAttributes,
+			Model model) {
+		Optional<Restaurant> optionalRestaurant = restaurantService.findRestaurantById(id);
 
-       return "restaurants/show";
-   }    
-   
+		if (optionalRestaurant.isEmpty()) {
+			redirectAttributes.addFlashAttribute("errorMessage", "店舗が存在しません。");
+
+			return "redirect:/restaurants";
+		}
+
+		Restaurant restaurant = optionalRestaurant.get();
+		Favorite favorite = null;
+		boolean isFavorite = false;
+
+		if (userDetailsImpl != null) {
+			User user = userDetailsImpl.getUser();
+			isFavorite = favoriteService.isFavorite(restaurant, user);
+
+			if (isFavorite) {
+				favorite = favoriteService.findFavoriteByRestaurantAndUser(restaurant, user);
+			}
+		}
+
+		model.addAttribute("restaurant", restaurant);
+		model.addAttribute("favorite", favorite);
+		model.addAttribute("isFavorite", isFavorite);
+
+		return "restaurants/show";
+	}
+
 }
